3926420a321496eab83dbd4a765a973eaa6294e2
rahulprakash05 <148333241+rahulprakash05@users.noreply.github.com>
2024-05-06 13:35:37 +0530

    new

--- s3/main.tf
+++ s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource ""aws_instance "rahul" {
+resource "aws_instance" "rahul" {
     instance_type = "t2.micro"
     ami = "ami-07caf09b362be10b8"
 }
